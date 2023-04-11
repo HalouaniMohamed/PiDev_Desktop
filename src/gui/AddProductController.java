83dd571ef0460c7181ec6b38a2203fc2d195c9b7
Halouani <Halouani142@gmail.com>
2023-04-11 13:44:26 +0000

    product's image upload

--- src/gui/AddProductController.java
+++ src/gui/AddProductController.java
@@ -7,6 +7,8 @@ package gui;
 
 import entities.Category;
 import entities.Product;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.Calendar;
@@ -22,7 +24,9 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.util.StringConverter;
+import org.apache.commons.io.FileUtils;
 import services.CategoryService;
 import services.ProductService;
 
@@ -43,6 +47,8 @@ public class AddProductController implements Initializable {
     private TextField tfQuantity;
     @FXML
     private ChoiceBox<Category> cbCategorie;
+    private String image;
+    private File selectedFile;
 
     ProductService ps = new ProductService();
     CategoryService cs = new CategoryService();
@@ -68,6 +74,10 @@ public class AddProductController implements Initializable {
         });
     }
 
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     @FXML
     private void addProduct(ActionEvent event) {
         if (tfName.getText().isEmpty() || tfPrice.getText().isEmpty() || tfQuantity.getText().isEmpty()) {
@@ -83,7 +93,7 @@ public class AddProductController implements Initializable {
             // get the values from the text fields
             double price = Double.parseDouble(tfPrice.getText());
             int quantity = Integer.parseInt(tfQuantity.getText());
-            String image = "aaa";
+
             String name = tfName.getText();
             String description = tfDescription.getText();
             // get the selected category from the ChoiceBox
@@ -99,6 +109,10 @@ public class AddProductController implements Initializable {
             // create a new Product object with the values
             Product p = new Product(name, description, price, image, quantity, date, date, category);
             CategoryService cs = new CategoryService();
+            if (selectedFile != null) {
+                uploadFile(selectedFile);
+                p.setImage(image);
+            }
             ps.add(p);
 
             // on success show alert that displays a success message then empty the textfields
@@ -113,4 +127,38 @@ public class AddProductController implements Initializable {
         }
 
     }
+
+    @FXML
+    private void handleUploadImage(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            // Do something with the selected file
+        }
+    }
+
+    private void uploadFile(File file) {
+        try {
+            String uniqueFileName = generateUniqueFileName(file.getName());
+            FileUtils.copyFile(file, new File("C:/Users/ALPHA/Documents/NetBeansProjects/Desktop/src/uploads/products/" + uniqueFileName));
+            // Set the image name in the product object
+            setImage(uniqueFileName);
+            // Save a copy of the image in the xampp htdocs directory
+
+            File destFile2 = new File("C:/xampp/htdocs/uploads/products/" + uniqueFileName);
+            FileUtils.copyFile(file, destFile2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateUniqueFileName(String fileName) {
+        // Generate a unique file name using a timestamp and the original file name
+        long timestamp = System.currentTimeMillis();
+        String[] parts = fileName.split("\\.");
+        String extension = parts[parts.length - 1];
+        return timestamp + "." + extension;
+    }
+
 }
