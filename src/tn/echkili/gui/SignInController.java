213f49a18206917f02f116e118f0260c42d2d30b
rayenbac <91440949+rayenbac@users.noreply.github.com>
2023-04-28 12:05:23 +0200

    api ++

--- src/tn/echkili/gui/SignInController.java
+++ src/tn/echkili/gui/SignInController.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,6 +26,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -40,7 +42,14 @@ import tn.echkili.entities.User;
 import tn.echkili.services.UserCRUD;
 import tn.echkili.tools.Connexion;
 import tn.echkili.tools.SessionManager;
-
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 
 /**
  * FXML Controller class
@@ -77,9 +86,10 @@ public class SignInController implements Initializable {
     private TextField numero;
     @FXML
     private PasswordField password_signup;
-    
     @FXML
     private TextField adresse;
+     @FXML
+    private ComboBox roles;
     @FXML
     private TextField email_signup;
     @FXML
@@ -186,6 +196,10 @@ public class SignInController implements Initializable {
         }
          
     }
+    
+    
+    
+    
      @FXML
     public void  signUp(){
       
@@ -247,14 +261,124 @@ public class SignInController implements Initializable {
          System.out.println(ex.getMessage());
     }
  }
-  
     
-     
+    public void resestpassword (ActionEvent event)throws IOException {
+         FXMLLoader loader = new FXMLLoader(getClass().getResource("ResetPass.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+    }
+  
+    void sendPassword(){
+        System.out.println("cxcccccccccccccccccc");
+                String query2="select * from user where email=? ";
+                String email1="empty";
+                 try {
+            PreparedStatement smt = cnx.prepareStatement(query2);
+            smt.setString(1, email_signin.getText());
+             ResultSet rs= smt.executeQuery();
+                if(rs.next()){
+                   email1=rs.getString("email");
+                     System.out.println(email1);
+                }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+                 sendMail(email1);
+    }
+    public void sendMail(String recepient){
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+        String myAccountEmail = "rayen.baccouch@esprit.tn";
+        String passwordd = "223JMT6774";
+       
+        Session session = Session.getInstance(properties, new Authenticator(){
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(myAccountEmail,passwordd);
+            } 
+        });
+        Message message = preparedMessage(session,myAccountEmail,recepient);
+        try{
+            Transport.send(message);
+             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Boite Mail");
+                alert.setHeaderText(null);
+                alert.setContentText("consulter votre boite mail !!");
+                alert.showAndWait();  
+           
+        }catch(Exception ex){
+            ex.printStackTrace();
+           
+        }
+               
+    }
+    
+    private Message preparedMessage(Session session, String myAccountEmail, String recepient){
+         String query2="select * from user where email=?";
+         String userEmail="null" ;
+         String pass="empty";
+        try {
+            PreparedStatement smt = cnx.prepareStatement(query2);
+            smt.setString(1, email_signin.getText());
+             ResultSet rs= smt.executeQuery();
+             System.out.println(rs);
+                if(rs.next()){
+                   pass=rs.getString("password");
+                   userEmail=rs.getString("email");                }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.print("c est en cours");
+        String text="Votre mot de pass est :"+pass+"";
+        String object ="Recupération de mot de passe";
+        Message message = new MimeMessage(session);
+        try{
+        message.setFrom(new InternetAddress(myAccountEmail));
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
+        message.setSubject(object);
+        String htmlcode ="<h1> "+text+" </h1> <h2> <b> </b2> </h2> ";
+        message.setContent(htmlcode, "text/html");
+         System.out.println("Message envoyeer");
+         
+           System.out.println(pass);
+           
+        return message;
+       
+        }catch(MessagingException ex){
+            ex.printStackTrace();
+        }
+    return null;
+    }
+     @FXML
+    void sendPaswword_btn(ActionEvent event)  {
+        try {
+        // Load the new FXML page
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ResetPassword.fxml"));
+        Parent root = loader.load();
+
+        // Show the new page in a new window
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+    
 
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        //dropShadowEffect();
-    }    
+   @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    roles.getItems().removeAll(roles.getItems());
+    roles.getItems().addAll("Médecin", "Patient");
+    roles.getSelectionModel().select("Choisir un role");
+}  
     
 }
